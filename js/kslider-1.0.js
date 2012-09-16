f96ce1ea9cbd5e0188e9d142eb518e838984a4c9
kevingermain <kevingermain@hotmail.fr>
2012-09-16 15:20:36 +0200

    Beta 1.2
    Version Beta 1.2

--- js/kslider-1.0.js
+++ js/kslider-1.0.js
@@ -1,12 +1,12 @@
 (function($){
 
-	 $.fn.kslider = function(options) {
+	$.fn.kslider = function(options) {
 
 		var kslider = $(this);
-		
-		//Ajout de la class kbig à la 1ère div du conteneur kslider
-		var kbig = $('div:eq(0)', kslider).addClass('kbig');
-		
+		kslider.append('<img class="kloading" src="http://dev.kevingermain.com/kslider/img/loading.gif">');
+		kslider.css('display', 'inline-block');
+		$('div', kslider).hide();
+
 		// Retourne une valeur MAX pour la taille (largeur ou hauteur) d'un selecteur
 		function max(selecteur, width) {
 			return Math.max.apply(Math, selecteur.map(function(){ 
@@ -16,240 +16,261 @@
 					return selecteur.outerHeight(true); 
 			}).get());
 		}
-		
-		//Récupération de la hauteur la plus haute entre les images
-		var kbigheightMax = max($('.kbig img', kslider), false);
-		
-		//Récupération de la largeur entre les images
-		var kbigwidthMax = max($('.kbig img', kslider), true);
-		
-		var ksmallheightMax = 0, ksmallwidthMax = 0, generateThumbs = 0;
-		if($('div:eq(1)', kslider).length == 0)
-		{
-			kbig.clone().appendTo(kslider);
-			$('div:eq(1)', kslider).removeClass().addClass('ksmall');
-			ksmallheightMax = kbigheightMax / 4;
-			ksmallwidthMax = kbigwidthMax / 4;
-			generateThumbs = 1;
-		}
-		else
-		{	
-			//Ajout de la class ksmall à la 2ème div du conteneur kslider
-			$('div:eq(1)', kslider).removeClass().addClass('ksmall');
-			ksmallheightMax = max($('div:eq(1) img', kslider), false);
-			ksmallwidthMax = max($('div:eq(1) img', kslider), true);
-		}
-		$('.ksmall', kslider).wrap("<div style='overflow:hidden;'></div>");
 
+		function getOptions(){
+			return options;
+		}
+		
+		
+		$(window).load(function() {  	
+			$('.kloading').remove();
+			$('div', kslider).show(); 
 			
-		//Paramètres par défaut
-		var defaults = {
-			width: kbigwidthMax,
-			height: kbigheightMax,
-			widthThumb: ksmallwidthMax,
-			heightThumb: ksmallheightMax,
-			nbImage: $('.kbig img', kslider).length,
-			speed: 500,
-			sameArea: false,
-			opacity:0.4,
-			nav:true
-		};
-		
-		//Application des paramètres de l'utilisateur
-		var options = $.extend(defaults, options);
-		
-
-		var ksmall = $('.ksmall', kslider).css('cursor','pointer');
-		var bordureksmallimg = ($('.ksmall img', kslider).outerWidth(true)-$('.ksmall img', kslider).width());
-
-		//Cacher toutes les grandes images sauf la 1ère
-		$('.kbig img', kslider).not(':first-child').hide();
-		
-		//Définition de la taille du slider complet : grand conteneur + petit conteneur
-		kslider.css({'width': options.width,'height' : options.height+options.heightThumb});
-		
-		//Définition de la taille du grand conteneur
-		kbig.css({'height': options.height,'width':options.width});
+			//Ajout de la class kbig à la 1ère div du conteneur kslider
+			var kbig = $('div:eq(0)', kslider).addClass('kbig');
+			var kbigimg = $('.kbig img', kslider);
 			
-		//Opérations sur les grandes images
-		$('.kbig img', kslider).css({'width':'auto', 'height':'auto', 'max-width': options.width, 'max-height':options.height, 'position':'absolute','cursor':'pointer'});
-		$('.kbig img', kslider).map(function(){
-			$(this).css({'margin-top': (options.height - $(this).outerHeight())/2, 
-							 'margin-bottom': (options.height - $(this).outerHeight())/2,
-							 'margin-right': (options.width - $(this).outerWidth())/2,
-							 'margin-left': (options.width - $(this).outerWidth())/2, });
-		});
-		
-		//Opérations sur les miniatures
-		var ksmallwidth = 0, indexWidth = [];
-		$('.ksmall img', kslider)
-		.css({'width':'auto', 
-			   'height':'auto', 
-				'max-width': options.widthThumb-bordureksmallimg, 
-				'max-height': options.heightThumb-bordureksmallimg,
-				'opacity': options.opacity,
-				'float':'left'})
-		.map(function(i){
-
-				if(options.sameArea)
-					ksmallwidth += options.widthThumb
-				else
-					ksmallwidth += $(this).outerWidth(true);
-				
-
-				indexWidth[i] = ksmallwidth;
-				
-				var differenceHauteur = options.heightThumb - $(this).outerHeight(true);
-				var differenceLargeur = options.widthThumb - $(this).outerWidth(true);
-				if(differenceHauteur > 0)
-					$(this).css({'padding-top': differenceHauteur/2, 
-									 'padding-bottom': differenceHauteur/2});
-				
-				if(differenceLargeur > 0 && options.sameArea)
-					$(this).css({'padding-right': differenceLargeur/2,
-									 'padding-left': differenceLargeur/2, });
-		});
-		$('.ksmall img:first-child', kslider).css('opacity','1');
-
-		//Définition de la taille du petit conteneur
-		if(ksmallwidth < options.width)
-			ksmall.css({'width': options.width, 'height':options.heightThumb, 'padding-left': (options.width - ksmallwidth) / 2+bordureksmallimg*2});
-		else
-			ksmall.css({'width': ksmallwidth+2, 'height':options.heightThumb});
-
-		//Ajout des flèches de navigation
-		if(options.nav)
-		{
-			kslider.append('<div class="kleft"></div><div class="kright"></div>');
-			$('.kleft, .kright', kslider)
-				.css({'border-color': 'transparent #777 transparent transparent', 
-						'cursor':'pointer',
-						'border-style': 'solid',
-						'position':'relative',
-						'float':'left',
-						'left':'-42px',
-						'margin-top':-options.heightThumb,
-						'border-width': options.heightThumb/2 + 'px 21px'});
-						
-			$('.kright', kslider)
-				.css({'border-color': 'transparent transparent transparent #777', 
-						'float':'right',
-						'left':'auto',
-						'right':'-42px'});
-		}
-		
-		ksmallimg = $('.ksmall img', kslider);
-		
-		//Move mouse hover thumnails - Bouger la souris au dessus des miniatures
-		var marginLeft = 0, posX = 0; var xp = 0;
-		ksmall.mousemove(function(e){ 
-			posX = e.pageX - kslider.offset().left; 
-			if(posX < 30)
-				posX = 0;
-			else if(posX > options.width-30)
-				posX = options.width;
-
-			marginLeft = -posX / ((options.width)/(ksmallwidth-options.width));
-		});
-		
-		if(posX < options.width && ksmallwidth > options.width)
-		{
-			setInterval(function(){
-				xp += ((marginLeft - xp) / 8);
-				ksmall.css({'margin-left': xp});
-			}, 10);
-		}
-
-		
-		// Hover on the thumbnails - Survol des miniatures
-		ksmallimg.hover(function(){
-			$(this).fadeTo(options.speed,1);
-		},
-		function(){
-			if(!$('.kbig img', kslider).eq($(this).index()).is(':visible'))
-				$(this).stop(true,true).fadeTo(options.speed/2, options.opacity);
-		}); 
-		
-		// click on the big image - click sur la grande image
-		$('.kbig img, .kright', kslider).on('click', function() {
-			sliding($('.kbig img:visible', kslider).index(), 1);
-		});
-		
-		$('.kleft', kslider).on('click', function() {
-			sliding($('.kbig img:visible', kslider).index(), 0);
-		});
-
-		function sliding (index, way)
-		{
-			var image = $('.kbig img', kslider).eq(index);
-			image.fadeOut(options.speed);
-			if(way == 1)
+			//Récupération de la hauteur la plus haute entre les images
+			var kbigheightMax = max(kbigimg, false);
+			
+			//Récupération de la largeur entre les images
+			var kbigwidthMax = max(kbigimg, true);
+			
+			var ksmallheightMax = 0, ksmallwidthMax = 0, generateThumbs = 0;
+			if($('div:eq(1)', kslider).length == 0)
 			{
-				if(image.next().length == 0)
-				{
-					$('.ksmall img', kslider).eq(index).fadeTo(options.speed/2,options.opacity);
-					$('.ksmall img', kslider).first().fadeTo(options.speed,1);
-					$('.kbig img', kslider).first().fadeIn(options.speed);
-					
-					if(ksmallwidth > options.width)
-						marginLeft = 0;
-				
-				}
-				else
-				{
-					image.next().fadeIn(options.speed);
-					$('.ksmall img', kslider).eq(index).fadeTo(options.speed/2,options.opacity);
-					$('.ksmall img', kslider).eq(index+1).fadeTo(options.speed,1);
-					var deplacement = -(indexWidth[index] + ($('.ksmall img', kslider).eq(index+1).outerWidth(true)-options.width)/2);
-					
-						if (deplacement < -(ksmallwidth-options.width))
-							marginLeft = -(ksmallwidth-options.width);
-						else if(deplacement < 0)
-							marginLeft = deplacement;
-						else if(deplacement > 0)
-							marginLeft = 0 ;
-				}
+				kbig.clone().appendTo(kslider);
+				$('div:eq(1)', kslider).removeClass().addClass('ksmall');
+				ksmallheightMax = kbigheightMax / 4;
+				ksmallwidthMax = kbigwidthMax / 4;
+				generateThumbs = 1;
 			}
 			else
-			{
-				if(image.prev().length == 0)
-				{
-					$('.ksmall img', kslider).eq(index).fadeTo(options.speed/2,options.opacity);
-					$('.ksmall img', kslider).last().fadeTo(options.speed,1);
-					$('.kbig img', kslider).last().fadeIn(options.speed);
-					
-					if(ksmallwidth > options.width)
-						marginLeft = options.width-ksmallwidth;
+			{	
+				//Ajout de la class ksmall à la 2ème div du conteneur kslider
+				$('div:eq(1)', kslider).removeClass().addClass('ksmall');
+				ksmallheightMax = max($('div:eq(1) img', kslider), false);
+				ksmallwidthMax = max($('div:eq(1) img', kslider), true);
+			}
+			var ksmall = $('.ksmall', kslider);
+			ksmall.wrap("<div style='overflow:hidden;'></div>").css('cursor','pointer');
+			var ksmallimg = $('.ksmall img', kslider);
+			
+			var defaults = {
+				width: kbigwidthMax,
+				height: kbigheightMax,
+				widthThumb: ksmallwidthMax,
+				heightThumb: ksmallheightMax,
+				speed: 500,
+				sameSize: false,
+				opacity:0.4,
+				nav:true
+			};
 				
+			if(getOptions())
+				options = $.extend(defaults, getOptions());
+			else
+				options = $.extend(defaults, options);
+
+			var bordureksmallimg = (ksmallimg.outerWidth(true)-ksmallimg.width());
+
+			//Cacher toutes les grandes images sauf la 1ère
+			kbigimg.not(':first-child').hide();
+			
+			//Fix IE7
+			$('.kbig img:first-child', kslider).css('display', 'block');
+			
+			//Asignation de la largeur et de la hauteur du slider complet : grand conteneur + petit conteneur
+			kslider.css({'width': options.width,'height' : options.height+options.heightThumb});
+			
+			//Définition de la taille du grand conteneur
+			kbig.css({'height': options.height,'width':options.width});
+				
+			//Opérations sur les grandes images
+			kbigimg.css({'width':'auto', 
+												  'height':'auto', 
+												  'max-width': options.width, 
+												  'max-height':options.height, 
+												  'position':'absolute',
+												  'cursor':'pointer'});
+												  
+			kbigimg.map(function(){
+				$(this).css({'margin-top': (options.height - $(this).outerHeight())/2, 
+								 'margin-bottom': (options.height - $(this).outerHeight())/2,
+								 'margin-right': (options.width - $(this).outerWidth())/2,
+								 'margin-left': (options.width - $(this).outerWidth())/2});
+			});
+			
+			//Opérations sur les miniatures
+			var ksmallwidth = 0, indexWidth = [];
+			ksmallimg
+			.css({'width':'auto', 
+					'height':'auto', 
+					'max-width': options.widthThumb-bordureksmallimg, 
+					'max-height': options.heightThumb-bordureksmallimg,
+					'opacity': options.opacity,
+					'float':'left'})
+			.map(function(i){
+
+					if(options.sameSize)
+						ksmallwidth += options.widthThumb;
+					else
+						ksmallwidth += $(this).outerWidth(true);
+					
+
+					indexWidth[i] = ksmallwidth;
+					
+					var differenceHauteur = options.heightThumb - $(this).outerHeight(true);
+					var differenceLargeur = options.widthThumb - $(this).outerWidth(true);
+					if(differenceHauteur > 0)
+						$(this).css({'padding-top': differenceHauteur/2, 
+										 'padding-bottom': differenceHauteur/2});
+					
+					if(differenceLargeur > 0 && options.sameSize)
+						$(this).css({'padding-right': differenceLargeur/2,
+										 'padding-left': differenceLargeur/2});
+			});
+			$('.ksmall img:first-child', kslider).css('opacity','1');
+
+			//Définition de la taille du petit conteneur
+			if(ksmallwidth < options.width)
+				ksmall.css({'width': options.width, 'height':options.heightThumb, 'padding-left': (options.width - ksmallwidth) / 2+bordureksmallimg*2});
+			else
+				ksmall.css({'width': ksmallwidth+2, 'height':options.heightThumb});
+
+			//Ajout des flèches de navigation
+			if(options.nav)
+			{
+				kslider.append('<div class="kleft"></div><div class="kright"></div>');
+				$('.kleft, .kright', kslider)
+					.css({'border-color': 'transparent #777 transparent transparent', 
+							'cursor':'pointer',
+							'display':'inline-block',
+							'border-style': 'solid',
+							'position':'relative',
+							'float':'left',
+							'left':'-42px',
+							'margin-top':-options.heightThumb,
+							'border-width': options.heightThumb/2 + 'px 21px'});
+							
+				$('.kright', kslider)
+					.css({'border-color': 'transparent transparent transparent #777', 
+							'float':'right',
+							'left':'auto',
+							'right':'-42px'});
+			}
+			
+			//Move mouse hover thumnails - Bouger la souris au dessus des miniatures
+			var marginLeft = 0, posX = 0; var xp = 0;
+			ksmall.mousemove(function(e){ 
+				posX = e.pageX - kslider.offset().left; 
+				if(posX < 30)
+					posX = 0;
+				else if(posX > options.width-30)
+					posX = options.width;
+
+				marginLeft = -posX / ((options.width)/(ksmallwidth-options.width));
+			});
+			
+			if(posX < options.width && ksmallwidth > options.width)
+			{
+				setInterval(function(){
+					xp += ((marginLeft - xp) / 8);
+					ksmall.css({'margin-left': xp});
+				}, 10);
+			}
+
+			
+			// Hover on the thumbnails - Survol des miniatures
+			ksmallimg.hover(function(){
+				$(this).fadeTo(options.speed,1);
+			},
+			function(){
+				if(!kbigimg.eq($(this).index()).is(':visible'))
+					$(this).stop(true,true).fadeTo(options.speed/2, options.opacity);
+			}); 
+			
+			// click on the big image - click sur la grande image
+			$('.kbig img, .kright', kslider).on('click', function() {
+				sliding($('.kbig img:visible', kslider).index(), 1);
+			});
+			
+			$('.kleft', kslider).on('click', function() {
+				sliding($('.kbig img:visible', kslider).index(), 0);
+			});
+
+			function sliding (index, way)
+			{
+				var image = kbigimg.eq(index);
+				image.fadeOut(options.speed);
+				if(way == 1)
+				{
+					if(image.next().length == 0)
+					{
+						ksmallimg.eq(index).fadeTo(options.speed/2,options.opacity);
+						ksmallimg.first().fadeTo(options.speed,1);
+						kbigimg.first().fadeIn(options.speed);
+						
+						if(ksmallwidth > options.width)
+							marginLeft = 0;
+					
+					}
+					else
+					{
+						image.next().fadeIn(options.speed);
+						ksmallimg.eq(index).fadeTo(options.speed/2,options.opacity);
+						ksmallimg.eq(index+1).fadeTo(options.speed,1);
+						var deplacement = -(indexWidth[index] + (ksmallimg.eq(index+1).outerWidth(true)-options.width)/2);
+						
+							if (deplacement < -(ksmallwidth-options.width))
+								marginLeft = -(ksmallwidth-options.width);
+							else if(deplacement < 0)
+								marginLeft = deplacement;
+							else if(deplacement > 0)
+								marginLeft = 0 ;
+					}
 				}
 				else
 				{
-					image.prev().fadeIn(options.speed);
-					$('.ksmall img', kslider).eq(index).fadeTo(options.speed/2,options.opacity);
-					$('.ksmall img', kslider).eq(index-1).fadeTo(options.speed,1);
-					var deplacement = -(indexWidth[index-2] + ($('.ksmall img', kslider).eq(index-1).outerWidth(true)-options.width)/2);
+					if(image.prev().length == 0)
+					{
+						ksmallimg.eq(index).fadeTo(options.speed/2,options.opacity);
+						ksmallimg.last().fadeTo(options.speed,1);
+						kbigimg.last().fadeIn(options.speed);
+						
+						if(ksmallwidth > options.width)
+							marginLeft = options.width-ksmallwidth;
 					
-						if (deplacement < -(ksmallwidth-options.width))
-							marginLeft = -(ksmallwidth-options.width);
-						else if(deplacement < 0)
-							marginLeft = deplacement;
-						else if(deplacement > 0)
-							marginLeft = 0 ;
+					}
+					else
+					{
+						image.prev().fadeIn(options.speed);
+						ksmallimg.eq(index).fadeTo(options.speed/2,options.opacity);
+						ksmallimg.eq(index-1).fadeTo(options.speed,1);
+						var deplacement = -(indexWidth[index-2] + (ksmallimg.eq(index-1).outerWidth(true)-options.width)/2);
+						
+							if (deplacement < -(ksmallwidth-options.width))
+								marginLeft = -(ksmallwidth-options.width);
+							else if(deplacement < 0)
+								marginLeft = deplacement;
+							else if(deplacement > 0)
+								marginLeft = 0 ;
+					}
 				}
 			}
-		}
-		
-		// click on the thumbnails - click sur les miniatures
-		ksmallimg.on('click', function() {
-
-			var index = $(this).index();
-			if(!$('.kbig img', kslider).eq(index).is(':visible'))
-			{
-				$('.kbig img', kslider).fadeOut(options.speed);
-				$('.ksmall img', kslider).not(this).fadeTo(options.speed, options.opacity);
-			}
-			$('.kbig img', kslider).eq(index).fadeIn(options.speed);
 			
+			// click on the thumbnails - click sur les miniatures
+			ksmallimg.on('click', function() {
+
+				var index = $(this).index();
+				if(!kbigimg.eq(index).is(':visible'))
+				{
+					kbigimg.fadeOut(options.speed);
+					ksmallimg.not(this).fadeTo(options.speed, options.opacity);
+				}
+				kbigimg.eq(index).fadeIn(options.speed);
+				
+			});
 		});
 	 };
 })(jQuery);
